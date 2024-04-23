6d374945dc81d187fd44edd80b3251a65411cd03
Youtmax <youtmax654@gmail.com>
2024-04-23 12:48:51 +0200

    Step 3 is done, tests passed

--- day01/main.go
+++ day01/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"SoftwareGoDay1/getevennumbers"
-	"fmt"
+	"SoftwareGoDay1/whatisyourname"
 )
 
 func main()  {
-	array := []int{1,0,19,17,16,8,13,24}
-
-fmt.Println(getevennumbers.GetEvenNumbers(array))
+	whatisyourname.WhatIsYourName()
 }
---
+++ day01/whatIsYourName/whatIsYourName.go
@@ -0,0 +1,19 @@
+package whatisyourname
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func WhatIsYourName() {
+	fmt.Println("What is your name ?")
+	fmt.Print("> ")
+	reader := bufio.NewReader((os.Stdin))
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Print("\nYour name is ", line)
+}
